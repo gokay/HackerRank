24283c4c14bb1f83404ccb72311a046ac4a17d2b
gokay.gursoy@gmail.com <gokay.gursoy@gmail.com>
2022-09-12 13:27:51 +0300

    Day 3: Intro to Conditional Statements

---
+++ Day3-Intro-to-conditionals/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static bool isOdd(int a)
+    {
+        return a % 2 == 0 ? false : true; 
+    }
+
+    public static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+        if (N >= 1 && N <= 100)
+        {
+            if (isOdd(N))
+            {
+                Console.WriteLine("Weird");
+            }
+
+            if (!isOdd(N) && (N >= 2 && N <= 5))
+            {
+                Console.WriteLine("Not Weird");
+            }
+
+            if (!isOdd(N) && (N >= 6 && N <= 20))
+            {
+                Console.WriteLine("Weird");
+            }
+
+            if (!isOdd(N) && (N > 20))
+            {
+                Console.WriteLine("Not Weird");
+            }
+        }
+    }
+}
---
+++ Solve-me-first/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+
+    static int solveMeFirst(int a, int b)
+    {
+        // 1 <= (a,b) <= 1000
+        // Hint: Type return a+b; below  
+        if ((a >= 1 && a <= 1000) && (b >= 1 && b <= 1000))
+            return a + b;
+        else
+            return 0;
+    }
+
+    static void Main(String[] args)
+    {
+        int val1 = Convert.ToInt32(Console.ReadLine());
+        int val2 = Convert.ToInt32(Console.ReadLine());
+        int sum = solveMeFirst(val1, val2);
+        Console.WriteLine(sum);
+    }
+}
