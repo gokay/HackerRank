dd4e80efaaf886419eb313d6aeb22028a70ce4f3
gokay.gursoy@gmail.com <gokay.gursoy@gmail.com>
2022-09-13 15:01:34 +0300

    Birthday-cake-candles

---
+++ Birthday-cake-candles/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        int tall = candles[0], count = 0;
+        //candles1 = candles.Sort();
+
+
+        foreach (int candle in candles)
+        {
+            if (tall > candle) continue;
+            if (tall < candle)
+            {
+                tall = candle;
+                continue;
+            }
+            if (tall == candle) count++;
+        }
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        Console.WriteLine(result);
+
+    }
+}
