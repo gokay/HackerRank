6332829192c5a42aea51ee6e176cfbd91bde5225
gokay.gursoy@gmail.com <gokay.gursoy@gmail.com>
2022-09-13 15:58:09 +0300

    Simple-array-sum

---
+++ Simple-array-sum/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'simpleArraySum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
+
+    public static int simpleArraySum(List<int> ar)
+    {
+        int sum = 0;
+
+        foreach (int i in ar)
+        {
+            if(i > 0 && i <= 1000)
+                sum += i;
+        }
+        return sum;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+        int result = 0;
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        if (arCount > 0 && arCount <= 1000)
+        {
+           result = Result.simpleArraySum(ar);
+        }
+
+        Console.WriteLine(result);
+
+    }
+}
